7d755da0673265772259fde2b1fbc6b1da0d0c91
yuri0r <macktrinz@yahoo.de>
2020-07-03 12:09:57 +0200

    changed color theme

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Hack:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -95,42 +95,43 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+        /* 8 normal colors */
+        [0] = "#101010", /* black   */
+        [1] = "#efa6a2", /* red     */
+        [2] = "#80c990", /* green   */
+        [3] = "#a69460", /* yellow  */
+        [4] = "#a3b8ef", /* blue    */
+        [5] = "#e6a3dc", /* magenta */
+        [6] = "#50cacd", /* cyan    */
+        [7] = "#808080", /* white   */
 
-	[255] = 0,
+        /* 8 bright colors */
+        [8]  = "#454545", /* black   */
+        [9]  = "#e0af85", /* red     */
+        [10] = "#5accaf", /* green   */
+        [11] = "#c8c874", /* yellow  */
+        [12] = "#ccaced", /* blue    */
+        [13] = "#f2a1c2", /* magenta */
+        [14] = "#74c3e4", /* cyan    */
+        [15] = "#c0c0c0", /* white   */
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+        /* special colors */
+        [256] = "#101010", /* background */
+        [257] = "#c0c0c0", /* foreground */
 };
-
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
+
+static unsigned int defaultitalic = 7;
+static unsigned int defaultunderline = 7;
 
 /*
  * Default shape of cursor
